4fb93839bec16e150008cc4107fd615b6ba46e81
Marian Marian <fenrir55@gmail.com>
2018-11-03 15:36:30 +0100

    Gotowy program, ma w sobie w błąd w prezentacji wyników przy title. Może w przyszłości to poprawie

--- linq_wpf/MainWindow.xaml.cs
+++ linq_wpf/MainWindow.xaml.cs
@@ -20,9 +20,21 @@ namespace linq_wpf
     /// </summary>
     public partial class MainWindow : Window
     {
+        ComicQueryManager comicQueryManager;
         public MainWindow()
         {
             InitializeComponent();
+            comicQueryManager = FindResource("comicQueryManager") as ComicQueryManager;
+            comicQueryManager.UpdateQueryResults(comicQueryManager.AvailableQueries[0]);
+        }
+
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(e.AddedItems.Count >=1 && e.AddedItems[0] is ComicQuery)
+            {
+                comicQueryManager.CurrentQueryResults.Clear();
+                comicQueryManager.UpdateQueryResults(e.AddedItems[0] as ComicQuery);
+            }
         }
     }
 }
